09853748553725fb4b3e95e7bff08f273cc1b3fb
pvtech <tech@golivelabs.io>
2014-12-19 10:05:05 -0800

    data

---
+++ myMVCLibrary.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * MVC - A simple MVC library featuring Models, views, and controllers
+ * Models are created by using a Model constructor
+ * Views are created by using a View constructor
+ *
+ * */
+
+// todo: Pending jsdocs
+
+function MVC() {
+    MVC.init();
+}
+
+MVC.init = function () {
+    // traverse the DOM looking for marker/attribute data-view to map controllers to views
+};
+
+MVC.Controller = function (controllerName, fn) {
+   // creates a controller and maps it out
+};
